96cc108a88e805b4910e41c778b8eeadc847b45c
David Okpare <24485401-DavidOkpare1@users.noreply.replit.com>
2024-02-22 20:44:42 +0000

    refactor to handle empty name strings

--- hello.go
+++ hello.go
@@ -3,8 +3,13 @@ package main // <-- Make sure it doesn't say 'main'.
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 func Hello(name string) string {
-	return "Hello, " + name + "!"
+	if name == "" {
+		name = "world"
+	}
+	return englishHelloPrefix + name + "!"
 }
 
 func main() {

9383e6516145969335f5916382061a3b8d4e8e3b
David Okpare <24485401-DavidOkpare1@users.noreply.replit.com>
2024-02-22 20:45:19 +0000

    added subtest and helper function

--- hello_test.go
+++ hello_test.go
@@ -4,8 +4,22 @@ package main // <-- Importing the right package here.
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("David")
-	want := "Hello, David!"
+	t.Run("saying hello to someone", func(t *testing.T) {
+		got := Hello("David")
+		want := "Hello, David!"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("saying hello when no name is given", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world!"
+		assertCorrectMessage(t, got, want)
+	})
+
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)

26840a444d45eb13eaf5e5be9745abeb9794f494
David Okpare <24485401-DavidOkpare1@users.noreply.replit.com>
2024-02-22 23:27:09 +0000

    refactor with switch case

--- hello.go
+++ hello.go
@@ -3,15 +3,35 @@ package main // <-- Make sure it doesn't say 'main'.
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
+const (
+	spanish            = "Spanish"
+	french             = "French"
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "world"
 	}
-	return englishHelloPrefix + name + "!"
+
+	return greetingPrefix(language) + name + "!"
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("David"))
+	fmt.Println(Hello("David", ""))
 }
